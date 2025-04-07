16ce54eb2ed06f4835bd1775c4fe972bdc143cee
ericmachmer@gmail.com <ericmachmer@gmail.com>
2025-04-06 20:13:34 -0700

    Define distance variable in speed function for correct calculation

--- exercises/02_basic_calculator/02_variables/src/lib.rs
+++ exercises/02_basic_calculator/02_variables/src/lib.rs
@@ -8,9 +8,9 @@
 pub fn speed(start: u32, end: u32, time_elapsed: u32) -> u32 {
     // TODO: define a variable named `distance` with the right value to get tests to pass
     //  Do you need to annotate the type of `distance`? Why or why not?
-
+    let distance: u32 = end - start;
     // Don't change the line below
-    distance / time_elapsed
+    distance / time_elapsed 
 }
 
 #[cfg(test)]
@@ -20,7 +20,7 @@ mod tests {
     #[test]
     fn case1() {
         assert_eq!(speed(0, 10, 10), 1);
-    }
+    }               
 
     #[test]
     fn case2() {
